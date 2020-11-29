5c5cf9bd8e2650753378cb7a85a3c35e0f7f9271
IvanSinesovsky <isinegovsky@gmail.com>
2020-11-29 17:29:13 +0300

    lab3 is ready to answer

--- src/bsu/rfe/java/group7/lab3/Sinegovsky/varA6/GornerTableModel.java
+++ src/bsu/rfe/java/group7/lab3/Sinegovsky/varA6/GornerTableModel.java
@@ -1,66 +1,62 @@
 package bsu.rfe.java.group7.lab3.Sinegovsky.varA6;
 
 import javax.swing.table.AbstractTableModel;
+
 @SuppressWarnings("serial")
 public class GornerTableModel extends AbstractTableModel {
+
     private Double[] coefficients;
     private Double from;
     private Double to;
     private Double step;
-    public GornerTableModel(Double from, Double to, Double step,
-                            Double[] coefficients) {
+
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients) {
         this.from = from;
         this.to = to;
         this.step = step;
         this.coefficients = coefficients;
     }
+
     public Double getFrom() {
         return from;
     }
+
     public Double getTo() {
         return to;
     }
+
     public Double getStep() {
         return step;
     }
+
     public int getColumnCount() {
-// В данной модели два столбца
-        return 2;
+        return 2; // В данной модели два столбца
     }
+
     public int getRowCount() {
-// Вычислить количество точек между началом и концом отрезка
-// исходя из шага табулирования
-        return new Double(Math.ceil((to-from)/step)).intValue()+1;
+        // Вычислить количество точек между началом и концом отрезка исходя из шага табулирования
+        return new Double(Math.ceil((to - from) / step)).intValue() + 1;
     }
     public Object getValueAt(int row, int col) {
-// Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
-        double x = from + step*row;
-        if (col==0) {
-// Если запрашивается значение 1-го столбца, то это X
-            return x;
+        // Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ * НОМЕР_СТРОКИ
+        double x = from + step * row;
+        if (col == 0) {
+            return x;               // Если запрашивается значение 1-го столбца, то это X
         } else {
-// Если запрашивается значение 2-го столбца, то это значение
-// многочлена
-            Double result = 0.0;
-// Вычисление значения в точке по схеме Горнера.
-// Вспомнить 1-ый курс и реализовать
-// ...
-            return result;
+            Double result = 0.0;    // Значение многочлена
+            return result;          // Вычисление значения в точке по схеме Горнера
         }
     }
+
     public String getColumnName(int col) {
         switch (col) {
             case 0:
-// Название 1-го столбца
-                return "Значение X";
+                return "Значение X";            // Название 1-го столбца
             default:
-// Название 2-го столбца
-                return "Значение многочлена";
+                return "Значение многочлена";   // Название 2-го столбца
         }
     }
     public Class<?> getColumnClass(int col) {
-// И в 1-ом и во 2-ом столбце находятся значения типа Double
         return Double.class;
     }
 }
-
